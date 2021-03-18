d516f1aa00bf8122ef4153b0bfcd5d97fe0deeb7
Tobias Hoppenthaler <tobias.hoppenthaler@gmail.com>
2021-03-18 22:09:29 +0100

    phase 1

--- PickApic/MainPage.xaml.cs
+++ PickApic/MainPage.xaml.cs
@@ -1,18 +1,63 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
+using System.IO;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace PickApic
 {
     public partial class MainPage : ContentPage
     {
+        public object PhotoPath { get; private set; }
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+         async void TakePhotoAsync(object sender, EventArgs e)
+        {
+            try
+            {
+                FileResult photo = await MediaPicker.CapturePhotoAsync();
+                await LoadPhotoAsync(photo);
+                Console.WriteLine($"CapturePhotoAsync COMPLETED: {PhotoPath}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"CapturePhotoAsync THREW: {ex.Message}");
+            }
+        }
+
+         async void PickPhotoAsync(object sender, EventArgs e)
+        {
+            try
+            {
+                FileResult photo = await MediaPicker.PickPhotoAsync();
+                await LoadPhotoAsync(photo);
+                Console.WriteLine($"CapturePhotoAsync COMPLETED: {PhotoPath}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"CapturePhotoAsync THREW: {ex.Message}");
+            }
+        }
+
+        private async Task LoadPhotoAsync(FileResult photo)
+        {
+            // canceled
+            if (photo == null)
+            {
+                PhotoPath = null;
+                return;
+            }
+            // save the file into local storage
+            var newFile = Path.Combine(FileSystem.CacheDirectory, photo.FileName);
+            using (var stream = await photo.OpenReadAsync())
+            using (var newStream = File.OpenWrite(newFile))
+                await stream.CopyToAsync(newStream);
+
+            PhotoPath = newFile;
         }
     }
 }
